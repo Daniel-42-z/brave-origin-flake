9c45b5294ccfb014ac26666ee95fa5ec8a1c4b0c
Daniel-42-z <daniel.zhang42@icloud.com>
2026-06-07 20:01:24 +0800

    fix makeOverridable

--- flake.nix
+++ flake.nix
@@ -6,71 +6,60 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
+        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
         versions = builtins.fromJSON (builtins.readFile ./versions.json);
         braveOriginData = versions."brave-origin";
       in
       {
-        packages.brave-origin =
-          (pkgs.callPackage "${nixpkgs}/pkgs/by-name/br/brave/make-brave.nix" { } {
+        packages.brave-origin = pkgs.lib.makeOverridable ({ vulkanSupport ? false, commandLineArgs ? "" }:
+          (pkgs.callPackage "${nixpkgs}/pkgs/by-name/br/brave/make-brave.nix" {
+            inherit vulkanSupport commandLineArgs;
+          } {
             pname = "brave-origin";
             version = braveOriginData.version;
             hash = braveOriginData.hash;
             url = braveOriginData.url;
-          }).overrideAttrs
-            (old: {
-              installPhase =
-                builtins.replaceStrings
-                  [
-                    "opt/brave.com/brave/brave-browser"
-                    "opt/brave.com/brave"
-                    "brave-browser,com.brave.Browser"
-                    "brave-browser.xml"
-                    "brave-browser.desktop"
-                    "com.brave.Browser.desktop"
-                    "/usr/bin/brave-browser-stable"
-                    "brave-browser.png"
-                  ]
-                  [
-                    "opt/brave.com/brave-origin/brave-origin"
-                    "opt/brave.com/brave-origin"
-                    "brave-origin,com.brave.Origin"
-                    "brave-origin.xml"
-                    "brave-origin.desktop"
-                    "com.brave.Origin.desktop"
-                    "/usr/bin/brave-origin-stable"
-                    "brave-origin.png"
-                  ]
-                  old.installPhase;
-
-              installCheckPhase =
-                builtins.replaceStrings [ "opt/brave.com/brave/brave" ] [ "opt/brave.com/brave-origin/brave" ]
-                  old.installCheckPhase;
-            });
+          }).overrideAttrs (old: {
+            installPhase = builtins.replaceStrings
+              [
+                "opt/brave.com/brave/brave-browser"
+                "opt/brave.com/brave"
+                "brave-browser,com.brave.Browser"
+                "brave-browser.xml"
+                "brave-browser.desktop"
+                "com.brave.Browser.desktop"
+                "/usr/bin/brave-browser-stable"
+                "brave-browser.png"
+              ]
+              [
+                "opt/brave.com/brave-origin/brave-origin"
+                "opt/brave.com/brave-origin"
+                "brave-origin,com.brave.Origin"
+                "brave-origin.xml"
+                "brave-origin.desktop"
+                "com.brave.Origin.desktop"
+                "/usr/bin/brave-origin-stable"
+                "brave-origin.png"
+              ]
+              old.installPhase;
+            
+            installCheckPhase = builtins.replaceStrings
+              [ "opt/brave.com/brave/brave" ]
+              [ "opt/brave.com/brave-origin/brave" ]
+              old.installCheckPhase;
+          })
+        ) {};
 
         packages.default = self.packages.${system}.brave-origin;
 
         devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.jq
-            pkgs.nix-prefetch
-          ];
+          buildInputs = [ pkgs.jq pkgs.nix-prefetch ];
         };
       }
-    )
-    // {
+    ) // {
       homeManagerModules.brave-browser = import ./modules/brave-browser.nix;
       homeManagerModules.default = self.homeManagerModules.brave-browser;
     };
